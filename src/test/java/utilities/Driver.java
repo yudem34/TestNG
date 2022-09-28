c80781bc5fbc221260f51b39f970a0914617108a
Yusuf Demir <yusufdemir8934@gmail.com>
2022-09-28 21:59:15 +0200

    V2

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -9,15 +9,34 @@ import java.time.Duration;
 public class Driver {
     static WebDriver driver;
 
-    public static WebDriver getDriver(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public static WebDriver getDriver() {
+        /*
+        Testleri calistirdigimizda her seferinde yeni driver olusturdugu icin her test methodu icin yeni
+        bir pencere (driver) aciyor.Eger driver'a bir deger atanmamissa yani driver==null ise
+        bir kere driver'i calistir diyerek bir kere if icini calistiracak ve driver artik bir kere
+         calistigi icin ve deger atandigi icin null olmayacak ve direk return edecek ve diger testlerimiz
+         ayni pencerede (driver) uzerinde calisacak
+         */
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
     }
 
-    public static void closeDriver(){
+    public static void closeDriver() {
+        if (driver != null) { // driver'a deger atanmissa
+            driver.close();
+            driver=null; // Kapandıktan sonraki acmaları garanti altına almak icin driver'i tekrar null yaptik
+        }
+    }
 
+    public static void quitDriver() {
+        if (driver!=null) {
+            driver.quit();
+            driver=null;
+        }
     }
 }
